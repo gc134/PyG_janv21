8cdd38adbe8b6930a6729dc77ef9bafe25229020
Gwendal Cueff <gwendal.cueff@inrae.fr>
2021-02-09 12:58:20 +0100

    1er commit de l'ensemble des fichiers du projet PyG

---
+++ final_150121/styleSheet.py
@@ -0,0 +1,47 @@
+styleSheet = """
+
+*{
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+QLabel{
+    border-style: double;
+    border-color: lightgrey;
+    border-width: 1px;
+    padding: 10px;
+    
+}
+
+QPushButton:hover {
+    background-color:rgb(97, 186, 238);
+}
+
+QPushButton:pressed {
+    background-color: white;
+}
+
+QPushButton#calcul_page4{
+    font-size: 28px;
+}
+
+QPushButton{
+    padding: 10px;
+}
+
+QTableView{
+    font-size: 20px;      
+ }
+ 
+
+QHeaderView { 
+	font-size: 20px; 
+}
+
+
+QGraphicsView#graphicsView1_page4{
+    background-image: url('./germinator.tif');
+    background-repeat: no-repeat;
+    margin-left: auto;
+}
+
+"""
